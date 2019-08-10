7c8cd68ad0d9e6bc50a22a7806c871a3ac1d3012
VirenPanchal <panchalviren00@gmail.com>
2019-08-10 18:05:33 +0530

    Added New Taskfile

---
+++ src/Stringfrequency.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Stringfrequency {
+	public static void main(String[]args)
+	{
+		int ci,i,j,k,l=0;
+		String str,str1;
+		char c,ch;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the String:");
+		str=sc.next();
+		i=str.length();
+		System.out.println(i);
+		for(c='A';c<='z';c++)
+		{
+			k=0;
+			for(j=0;j<i;j++)
+			{
+				ch=str.charAt(j);
+				if(ch==c)
+				{
+					k++;
+				}
+			}
+			if(k>0)
+			{
+				System.out.println(c+"  frequency: "+k +"  times " );
+			}
+		}
+	}
+
+}
